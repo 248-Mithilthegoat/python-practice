199809e482382400d386ce3fad24e822bec4be28
248-Mithilthegoat <jokestermithil@gmail.com>
2025-06-30 15:28:52 -0700

    Updated to print a message and ask for a new number when a invalid input is inputted.

--- factorial.py
+++ factorial.py
@@ -1,17 +1,26 @@
-# Main code
-def main():
-    # Getting user input
-    x = int(input("Enter a number: "))
-    # Factorial function
-    factorial_implementation(x)
+def get_input():
+    x = int(input("Enter a positive integer: "))
+    if x < 0:
+        return False, x
+    return True, x
 
-# Definition of factorial implementation function
-def factorial_implementation(n):
+
+def factorial_implementation(x):
     factorial = 1
-    # Loop that causes the given number to multiply continuously
-    for i in range(1, n+1):
-        factorial = factorial*i
-    print(factorial)
-# Executing the program
+    for i in range(1, x+1):
+        factorial *= i
+    return factorial
+
+def main():
+    is_valid = False
+
+    while(not is_valid):
+        is_valid, x = get_input()
+        if not is_valid:
+            print(f"{x} is not positive.")
+    result = factorial_implementation(x)
+    print(x, "factorial =", result)
+
+
 main()
 
