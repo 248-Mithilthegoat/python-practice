4a40faf0252a6b9dbbf589d7ee479cb16b07d004
248-Mithilthegoat <jokestermithil@gmail.com>
2025-06-30 11:29:20 -0700

    Updated to include the def function.

--- sum_of_natural_numbers.py
+++ sum_of_natural_numbers.py
@@ -1,8 +1,13 @@
-# Collecting user input
-x = int(input("Please enter a positive integer: "))
-sum = 0
+def main():
+    # Collecting user input
+    x = int(input("Enter a number: "))
+    sum_natural_numbers(x)
+
 # Loop that continuously adds down to 0
-for i in range(1, x+1):
-    # Changes the value of sum variable to the sum of the natural numbers
-    sum += 1
-print(sum)
+def sum_natural_numbers(n):
+    sum = 0
+    for i in range(1, n+1):
+        # Changes the value of sum variable to the sum of the natural numbers
+        sum += i
+    print(sum)
+main()
