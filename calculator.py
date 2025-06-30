18b11f4b8d64a18cfc7ca15e873e66cfa7cead67
248-Mithilthegoat <jokestermithil@gmail.com>
2025-06-30 11:26:44 -0700

    Created a program that collects two numbers and an operator from the user and calculates that arithmetic

--- calculator.py
+++ calculator.py
@@ -1,9 +1,11 @@
 def main():
+   # Getting user input for the two numbers and operator
     x = int(input("Enter number x: "))
     y = int(input("Enter number y: "))
     operator = str(input("Enter the operator: "))
     calculate(x, y, operator)
 def calculate(num1, num2, operator):
+    # Checking which operator and continuing with that operation
     if operator == "+":
         print(num1 + num2)
     elif operator == "-":
@@ -11,6 +13,7 @@ def calculate(num1, num2, operator):
     elif operator == "*":
         print(num1 * num2)
     elif operator == "/":
+        # Checking if there is a division by zero, and if so printing "Divide by zero error"
         if num2 != 0:
             print(num1 / num2)
         elif num2 == 0:
