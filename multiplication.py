4b783bfe8b939ef178becdfea89190adb0ede88b
248-Mithilthegoat <jokestermithil@gmail.com>
2025-06-30 11:27:47 -0700

    Updated so that it uses the def function, and has comments.

--- multiplication.py
+++ multiplication.py
@@ -1,3 +1,10 @@
-x = int(input("Enter a number: "))
-for i in range(1, 11):
-    print(x, "x", i, "=", x*i)
+def main():
+    # Collecting user input
+    x = int(input("Enter a number: "))
+    # Function for printing multiplication table of x
+    multiplication(x)
+# Definition of function for printing multiplication table
+def multiplication(n):
+    for i in range(1, 11):
+        print(n, "x", i, "=", n*i)
+main()
