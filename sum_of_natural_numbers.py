919616f0272323287ba24a66e4120632f57ebbfc
248-Mithilthegoat <jokestermithil@gmail.com>
2025-06-30 13:04:03 -0700

    Updated to print a message when a invalid input is inputted.

--- sum_of_natural_numbers.py
+++ sum_of_natural_numbers.py
@@ -1,28 +1,25 @@
-def main():
-    is_valid == True
-    # Collecting user input
-    x = int(input("Enter a number: "))
-    # Checking if input is valid, and if not printing a message accordingly
-    get_input(x)
-    # Executing a function to add the natural numbers consecutively
-    if is_valid is False:
-        pass
-    elif is_valid is True:
-        sum_natural_numbers(x)
-# Defining the function to check if the number given by the user is valid
-def get_input(n):
-    # Conditional to print a message if the user input is invalid
-    
-    if n <= 0:
-        print("Enter a number greater than 0. ")
-        return is_valid is False
-    elif n > 0:
-        return n
-# Defining the function to add the natural nubers consecutively
-def sum_natural_numbers(a):
+def get_input():
+    x = int(input("PLease enter a positive integer: "))
+    if x < 0:
+        return False, x
+    return True, x
+
+
+
+def sum_of_numbers(x):
     sum = 0
-    for i in range(1, a+1):
+    for i in range(1, x+1):
         sum += i
-    print(sum)
-# Executing the entire program
+    return sum
+
+
+def main():
+    is_valid = False
+    while(not is_valid):
+        is_valid, x = get_input()
+        if not is_valid:
+            print(f"{x} is not a positive integer")
+        
+    result = sum_of_numbers(x)
+    print(f"The sum of {x} natural numbers is {result}")
 main()
