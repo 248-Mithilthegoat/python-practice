37f76a4b4e283d74f4d50a34db8fa231ba739d48
248-Mithilthegoat <jokestermithil@gmail.com>
2025-06-30 12:28:48 -0700

    These are the code that I am talking about.

--- even_odd_checker.py
+++ even_odd_checker.py
@@ -1,8 +1,17 @@
 def main():
     # Collect user input
     x = int(input("Enter a number: "))
-    # Checking if even or odd
+    # Function to check if user input is valid, if invalid prints a message
+    get_input(x)
+    # Function to check if user input is even/odd
     even_odd_checker(x)
+# Defining the function to check for invalid input and if input is invalid printing a message
+def get_input(a):
+    if a <= 0:
+        print("Enter a number greater than 0")
+    elif a > 0:
+        return a
+# Defining a function to check if the number given by the user is even/odd
 def even_odd_checker(n):
     if n%2== 0:
         print(n,"is even.")
--- sum_of_natural_numbers.py
+++ sum_of_natural_numbers.py
@@ -1,15 +1,28 @@
 def main():
+    is_valid == True
+    # Collecting user input
     x = int(input("Enter a number: "))
+    # Checking if input is valid, and if not printing a message accordingly
     get_input(x)
-    sum_natural_numbers(x)
+    # Executing a function to add the natural numbers consecutively
+    if is_valid is False:
+        pass
+    elif is_valid is True:
+        sum_natural_numbers(x)
+# Defining the function to check if the number given by the user is valid
 def get_input(n):
+    # Conditional to print a message if the user input is invalid
+    
     if n <= 0:
-        n =int(input("Enter a number greater than 0: "))
+        print("Enter a number greater than 0. ")
+        return is_valid is False
     elif n > 0:
         return n
+# Defining the function to add the natural nubers consecutively
 def sum_natural_numbers(a):
     sum = 0
     for i in range(1, a+1):
         sum += i
     print(sum)
+# Executing the entire program
 main()
