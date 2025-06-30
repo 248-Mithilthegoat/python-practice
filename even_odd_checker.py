2458a50ccdce4947213425310ac59e6c8aa94ddd
248-Mithilthegoat <jokestermithil@gmail.com>
2025-06-30 14:14:47 -0700

    This is the file.

--- even_odd_checker.py
+++ even_odd_checker.py
@@ -1,20 +1,29 @@
-def main():
-    # Collect user input
-    x = int(input("Enter a number: "))
-    # Function to check if user input is valid, if invalid prints a message
-    get_input(x)
-    # Function to check if user input is even/odd
-    even_odd_checker(x)
-# Defining the function to check for invalid input and if input is invalid printing a message
-def get_input(a):
-    if a <= 0:
-        print("Enter a number greater than 0")
-    elif a > 0:
-        return a
-# Defining a function to check if the number given by the user is even/odd
-def even_odd_checker(n):
-    if n%2== 0:
-        print(n,"is even.")
+def get_input():
+    x = int(input("Enter a positive integer: "))
+    if x < 0:
+        return False, x
+    return True, x
+even = "even"
+odd = "odd"
+y = None
+def even_odd_checker(x):
+    if x % 2 == 0:
+        y = "even"
     else:
-        print(n,"is odd.")
+        y = "odd"
+    return y
+
+
+
+def main():
+    is_valid = False
+
+    while(not is_valid):
+        is_valid, x = get_input()
+
+        if (not is_valid):
+            print(f"{x} is not a positive integer")
+    print(f"{x} is {y}")
+
+
 main()
