717ddbf6e722d51ef88a6b9ba7a467815e8ba7d9
248-Mithilthegoat <jokestermithil@gmail.com>
2025-07-01 12:39:50 -0700

    Created two programs: prime_checker to check if number given by user is prime/composite. And temperature_converter to convert a temperature given by the user from Celsius to Fahrenheit or vice versa.

--- prime_checker.py
+++ prime_checker.py
@@ -1,9 +1,11 @@
+# Defining function to collect input and validate and return the value of a and validation
 def get_input():
     a = int(input("Enter a positive integer: "))
     if a <= 1:
         return False, a
     return True, a
 
+# Definition of check_prime function to check if a given number is prime/composite
 def check_prime(n):
     for i in range(2, n):
         if n % i == 0:
@@ -12,6 +14,7 @@ def check_prime(n):
     else:
         print(n, "is prime")
 
+# The main program with all functions
 def main():
     is_valid = False
     while(not is_valid):
@@ -20,4 +23,5 @@ def main():
             print(a, "is not greater than 1.")
     check_prime(a)
 
+# Executing the main function to execute the entire program
 main()
