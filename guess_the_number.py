ce0ef50c1ca5ae335fd7faac28ad169be7358e3f
248-Mithilthegoat <jokestermithil@gmail.com>
2025-07-04 14:20:28 -0700

    Updated the programs to have comments.

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,6 +1,6 @@
-import random
-a = range(1, 101)
-def guess(choice):
+import random # Importing the random function to have access to random.choice
+a = range(1, 101) # Creating a variable with a range of 1 - 100
+def guess(choice): # Defining a function to print whether the users guess needs to be higher or lower or if it is correct
     num = int(input("Guess a number from 1-100: "))
     if num < choice:
             print("Higher")
@@ -11,7 +11,7 @@ def guess(choice):
     elif num == choice:
             print("You did it!")
             return True
-def play_again():
+def play_again(): # Defining a function to ask if the user wants to play again
     print("Do you want to play again?")
     print("1) Yes")
     print("2) No")
@@ -21,7 +21,7 @@ def play_again():
     return False
 def main():
     play = True
-    while play:
+    while play: # Creating a while loop to ask if the user wants to play again, if they do then it will restart the game, if they don't then it will simply stop
         correct = False
         attempts = 0
         choice = random.choice(a)
