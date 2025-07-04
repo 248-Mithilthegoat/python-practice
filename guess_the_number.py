c9ba97126d2f129a6de94cd0ebe1a86d01049f88
248-Mithilthegoat <jokestermithil@gmail.com>
2025-07-03 22:16:47 -0700

    This is the game.

--- guess_the_number.py
+++ guess_the_number.py
@@ -11,13 +11,26 @@ def guess():
             return False
     elif num == choice:
             print("You did it!")
-            correct = True
             return True
+def play_again():
+    print("Do you want to play?")
+    print("1) Yes")
+    print("2) No")
+    again = int(input("Make your choice: "))
+    if again == 1:
+          return True
+    return False
 def main():
     correct = False
+    play = False
+    play = play_again()
     attempts = 0
     while (not correct):
+        correct = guess()
         attempts += 1
-        guess()
-    print(attempts)
+    print(attempts, "attempts")
+    while play:
+        correct = guess()
+        if play:
+            play = play_again()
 main()
