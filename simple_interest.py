0e0b9c1e84b2ec43424ded4292ba7266a3fb8b71
248-Mithilthegoat <jokestermithil@gmail.com>
2025-07-04 18:14:54 -0700

    Updated to have input validation.

--- simple_interest.py
+++ simple_interest.py
@@ -1,30 +1,26 @@
-principal = int(input("Enter the principal amount: ")) # Collecting the principal amount
-interest_rate = float(input("Enter the interest rate: ")) # Collecting the interest rate per year
-time = int(input("Enter how many years the deposit has been in the account: ")) # Collecting the time that the principal has been in the account
-def check_input(x):
-    if x < 0:
-        return False, x
-    return True, x
+def check_input(x): # Defining the function to check if user input is valid
+    if x < 0: 
+        return False
+    return True
 def main():
-    principal = int(input("Enter the principal amount: ")) # Collecting the principal amount
-    interest_rate = float(input("Enter the interest rate: ")) # Collecting the interest rate per year
-    time = int(input("Enter how many years the deposit has been in the account: ")) # Collecting the time that the principal has been in the account
-    is_valid = False
-    check_input(principal)
-    while not is_valid:
-        print("That is not a valid principal.")
+    is_principal_valid = False
+    is_interest_valid = False
+    is_term_valid = False
+    while not is_principal_valid: # While loops and if conditionals to check if user inputs are valid or not
         principal = int(input("Enter the principal amount: "))
-    check_valid = False
-    check_input(interest_rate)
-    while not check_valid:
-        print("That is not a valid interest rate.")
+        is_principal_valid = check_input(principal)
+        if not is_principal_valid:
+            print("Principal that you entered is not valid!")
+    while not is_interest_valid:
         interest_rate = float(input("Enter the interest rate: "))
-    isn_valid = False
-    check_input(time)
-    while not isn_valid:
-        print("That is not a valid time.")
+        is_interest_valid = check_input(interest_rate)
+        if not is_interest_valid:
+            print("Interest that you entered is not valid")
+    while not is_term_valid:
         time = int(input("Enter how many years the deposit has been in the account: "))
-        simple_interest = 0
+        is_term_valid = check_input(time)
+        if not is_term_valid:
+            print("Time is not valid")
     simple_interest = (principal * interest_rate * time) / 100 # Calculating the simple interest
     print(f"The simple interest is ${simple_interest}. The total amount is ${principal + simple_interest}.") # Printing the simple interest and the total the deposit is worth
 main()
